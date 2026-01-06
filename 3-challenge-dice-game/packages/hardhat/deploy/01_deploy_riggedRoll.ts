a3a850c75f693b77c62ad301ff31855145c1981d
MateoDi9z <dzmateozd@protonmail.com>
2026-01-06 15:54:49 -0300

    challenge 3 done

---
+++ 3-challenge-dice-game/packages/hardhat/deploy/01_deploy_riggedRoll.ts
@@ -0,0 +1,33 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat/";
+import { DiceGame, RiggedRoll } from "../typechain-types";
+
+const deployRiggedRoll: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployer } = await hre.getNamedAccounts();
+  const { deploy } = hre.deployments;
+
+  const diceGame: DiceGame = await ethers.getContract("DiceGame");
+  const diceGameAddress = await diceGame.getAddress();
+
+  // Uncomment to deploy RiggedRoll contract
+  await deploy("RiggedRoll", {
+    from: deployer,
+    log: true,
+    args: [diceGameAddress],
+    autoMine: true,
+  });
+
+  const riggedRoll: RiggedRoll = await ethers.getContract("RiggedRoll", deployer);
+
+  // Please replace the text "Your Address" with your own address.
+  try {
+    await riggedRoll.transferOwnership("0x26583527B405434313EC0A88F629Fb99B42E1e6D");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export default deployRiggedRoll;
+
+deployRiggedRoll.tags = ["RiggedRoll"];
